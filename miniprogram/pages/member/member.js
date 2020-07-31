6b6e51ca7b4f442433da9de7ff57838d79af2fe7
Jater <lijunzhuo@yupaopao.cn>
2020-07-31 10:26:09 +0800

    ljz_crop

---
+++ miniprogram/pages/member/member.js
@@ -0,0 +1,14 @@
+const app = getApp()
+
+Page({
+  data: {
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+  }
+})
